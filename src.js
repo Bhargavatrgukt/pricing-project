5a01407f3f6978044179ac741e54f75c66b895b0
Bhargav <bhargavatrgukt@gmail.com>
2024-11-02 16:15:42 +0530

    card alignments

--- src.js
+++ src.js
@@ -7,12 +7,12 @@ console.log(Toggle.checked)
 
 Toggle.addEventListener("change",()=>{
     if(Toggle.checked){
-       price1.textContent="$19.99";
-       price2.textContent="$24.99";
-       price3.textContent="$39.99";
+       price1.textContent="19.99";
+       price2.textContent="24.99";
+       price3.textContent="39.99";
     }else{
-        price1.textContent="$199.99";
-        price2.textContent="$249.99";
-        price3.textContent="$399.99";
+        price1.textContent="199.99";
+        price2.textContent="249.99";
+        price3.textContent="399.99";
     }
 })
